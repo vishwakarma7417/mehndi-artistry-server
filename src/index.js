3812ea77a96e27b21c051d7ed9da1e000a71dc29
vishwakarma7417 <asaman9568@gmail.com>
2025-05-12 17:10:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://mehndi-artistiry.vercel.app","http://localhost:5174","https://mehndi-artistry-admin.vercel.app/"],
+  origin: ["http://localhost:5173", "https://mehndi-artistiry.vercel.app","http://localhost:5174","https://mehndi-artistry-admin.vercel.app"],
   credentials: true,
   methods: ["GET", "POST","PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
